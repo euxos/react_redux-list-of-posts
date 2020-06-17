3a4cb718c43dc73cabf934393fd63cf49bbb076a
euxos <eugen.ostrozhinsky@gmail.com>
2020-06-17 08:44:58 +0300

    Updates

---
+++ precache-manifest.108f7473d8e9b5803005b0763749a775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd857a1421b8574b3b70e8757698572",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49b3c153320e51289091",
+    "url": "./static/css/main.11de18ef.chunk.css"
+  },
+  {
+    "revision": "5e709a0770df3830c904",
+    "url": "./static/js/2.becfab72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.becfab72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b3c153320e51289091",
+    "url": "./static/js/main.51dd1bcb.chunk.js"
+  },
+  {
+    "revision": "bd9b317a4d0439b81bbd",
+    "url": "./static/js/runtime-main.78f69ce3.js"
+  }
+]);
